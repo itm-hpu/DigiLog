f1a57527a9922659c643b6cddd117fd9a4842d3d
jeongyk89 <jake8967@hotmail.com>
2019-04-30 17:59:32 +0200

    Async problem left

---
+++ KafkaManager/DigiLogKafka/Manager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using System.Windows.Forms;
+
+
+namespace DigiLogKafka
+{
+    public class Manager
+    {
+        public Manager()
+        {
+        }
+        public void SendMessage(string kafkaAddress, string topicName, string message)
+        {
+            try
+            {
+                var options = new KafkaOptions(new Uri(kafkaAddress));
+                var router = new BrokerRouter(options);
+                var client = new Producer(router);
+
+                client.SendMessageAsync(topicName, new[] { new KafkaNet.Protocol.Message(message + " " + DateTime.Now) }).Wait();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Concat(ex.InnerException, ex.Message));
+            }
+            finally
+            {
+                Thread.CurrentThread.Abort();
+            }
+        }
+    }
+}
--- KafkaManager/Main/MainWindow.xaml.cs
+++ KafkaManager/Main/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,7 +14,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using Manager;
+
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+//using DigiLogKafka;
 
 namespace Main
 {
@@ -22,12 +28,15 @@ namespace Main
     /// </summary>
     public partial class MainWindow : Window
     {
+        //DigiLogKafka.Manager mManager;
+
         public MainWindow()
         {
-            InitializeComponent();
-            txtKafkaPServer.Text = "PLAINTEXT://kafkabroker.northeurope.cloudapp.azure.com:9092";
-            txtKafkaPTopic.Text = "Testtopic11";
+            InitializeComponent();            
 
+            txtKafkaPServer.Text = "http://localhost:9092";
+            txtKafkaPTopic.Text = "test";            
+            
             if (chbKafkaSetting.IsChecked == true)
             {
                 txtKafkaCServer.Text = txtKafkaPServer.Text;
@@ -57,6 +66,73 @@ namespace Main
                 txtKafkaCTopic.Text = "";
 
             }
+        }
+
+        private async void BtnSend_Click(object sender, RoutedEventArgs e)
+        {
+            //DigiLogKafka.Manager mManager = new Manager();
+
+            //this.Dispatcher.Invoke(() => { SendMessage(txtKafkaPServer.Text, txtKafkaPTopic.Text, txtMessage.Text); });
+
+            //Thread t = new Thread(() => { SendMessage(txtKafkaPServer.Text, txtKafkaPTopic.Text, txtMessage.Text); });
+            //t.Start();
+
+            SendMessage(txtKafkaPServer.Text, txtKafkaPTopic.Text, txtMessage.Text);            
+
+        }
+
+        public void SendMessage(string kafkaAddress, string topicName, string message)
+        {
+            try
+            {
+                var options = new KafkaOptions(new Uri(kafkaAddress));
+                var router = new BrokerRouter(options);
+                var client = new Producer(router);
+
+                client.SendMessageAsync(topicName, new[] { new KafkaNet.Protocol.Message(message + " " + DateTime.Now) }).Wait();               
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Concat(ex.InnerException, ex.Message));
+            }
+            finally
+            {
+                Thread.CurrentThread.Abort();
+            }
+        }
+
+        public void GetMessages(string kafkaAddress, string topicName)
+        {
+            var options = new KafkaOptions(new Uri(kafkaAddress));
+            var router = new BrokerRouter(options);
+
+            var consumer = new Consumer(new ConsumerOptions(topicName, router));
+
+            try
+            {                
+                foreach (var message in consumer.Consume())
+                {
+                    if (txtMessage.Text.Length > 0)
+                    {
+                        txtMessage.AppendText(Environment.NewLine);
+                    }
+                    txtMessage.AppendText("[" + DateTime.Now + "] " + Encoding.UTF8.GetString(message.Value));                    
+                }
+            }
+            catch (ThreadAbortException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Concat(ex.InnerException, ex.Message));
+            }
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            GetMessages(txtKafkaCServer.Text, txtKafkaCTopic.Text);
         }
     }
 }
