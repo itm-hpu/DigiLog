4587b57037693a8de64d8b47fa30a5b31032d297
Yongkuk Jeong (Jake) <jake8967@hotmail.com>
2019-05-02 13:34:58 +0200

    ip address is changed

--- KafkaManager/MainForm/Main.Designer.cs
+++ KafkaManager/MainForm/Main.Designer.cs
@@ -38,6 +38,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.tabConsumer = new System.Windows.Forms.TabPage();
+            this.btnEnd = new System.Windows.Forms.Button();
             this.txtCMessage = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.txtCTopic = new System.Windows.Forms.TextBox();
@@ -51,7 +52,6 @@
             this.chbConsumer = new System.Windows.Forms.CheckBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.txtToolTip = new System.Windows.Forms.ToolStripStatusLabel();
-            this.btnEnd = new System.Windows.Forms.Button();
             this.tabMain.SuspendLayout();
             this.tabProducer.SuspendLayout();
             this.tabConsumer.SuspendLayout();
@@ -67,7 +67,7 @@
             this.tabMain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabMain.Name = "tabMain";
             this.tabMain.SelectedIndex = 0;
-            this.tabMain.Size = new System.Drawing.Size(772, 442);
+            this.tabMain.Size = new System.Drawing.Size(662, 479);
             this.tabMain.TabIndex = 0;
             // 
             // tabProducer
@@ -83,17 +83,17 @@
             this.tabProducer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabProducer.Name = "tabProducer";
             this.tabProducer.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabProducer.Size = new System.Drawing.Size(764, 416);
+            this.tabProducer.Size = new System.Drawing.Size(654, 453);
             this.tabProducer.TabIndex = 0;
             this.tabProducer.Text = "Producer";
             this.tabProducer.UseVisualStyleBackColor = true;
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(617, 364);
+            this.btnSend.Location = new System.Drawing.Point(529, 394);
             this.btnSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(131, 28);
+            this.btnSend.Size = new System.Drawing.Size(112, 30);
             this.btnSend.TabIndex = 6;
             this.btnSend.Text = "Send message";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -101,55 +101,55 @@
             // 
             // txtPMessage
             // 
-            this.txtPMessage.Location = new System.Drawing.Point(23, 114);
+            this.txtPMessage.Location = new System.Drawing.Point(20, 124);
             this.txtPMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtPMessage.Multiline = true;
             this.txtPMessage.Name = "txtPMessage";
-            this.txtPMessage.Size = new System.Drawing.Size(726, 246);
+            this.txtPMessage.Size = new System.Drawing.Size(623, 266);
             this.txtPMessage.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 90);
+            this.label3.Location = new System.Drawing.Point(17, 98);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.Size = new System.Drawing.Size(55, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Messages";
             // 
             // txtPTopic
             // 
-            this.txtPTopic.Location = new System.Drawing.Point(114, 46);
+            this.txtPTopic.Location = new System.Drawing.Point(98, 50);
             this.txtPTopic.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtPTopic.Name = "txtPTopic";
-            this.txtPTopic.Size = new System.Drawing.Size(635, 21);
+            this.txtPTopic.Size = new System.Drawing.Size(545, 20);
             this.txtPTopic.TabIndex = 3;
-            this.txtPTopic.Text = "testYK";
+            this.txtPTopic.Text = "test";
             // 
             // txtPServer
             // 
-            this.txtPServer.Location = new System.Drawing.Point(114, 14);
+            this.txtPServer.Location = new System.Drawing.Point(98, 15);
             this.txtPServer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtPServer.Name = "txtPServer";
-            this.txtPServer.Size = new System.Drawing.Size(635, 21);
+            this.txtPServer.Size = new System.Drawing.Size(545, 20);
             this.txtPServer.TabIndex = 2;
-            this.txtPServer.Text = "http://localhost:9092";
+            this.txtPServer.Text = "http://130.237.77.240:9092";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 49);
+            this.label2.Location = new System.Drawing.Point(17, 53);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 12);
+            this.label2.Size = new System.Drawing.Size(63, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Topic name";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 17);
+            this.label1.Location = new System.Drawing.Point(17, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 12);
+            this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Kafka server";
             // 
@@ -171,70 +171,81 @@
             this.tabConsumer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabConsumer.Name = "tabConsumer";
             this.tabConsumer.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabConsumer.Size = new System.Drawing.Size(764, 416);
+            this.tabConsumer.Size = new System.Drawing.Size(654, 453);
             this.tabConsumer.TabIndex = 1;
             this.tabConsumer.Text = "Consumer";
             this.tabConsumer.UseVisualStyleBackColor = true;
             // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(590, 13);
+            this.btnEnd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(50, 24);
+            this.btnEnd.TabIndex = 17;
+            this.btnEnd.Text = "End";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
             // txtCMessage
             // 
-            this.txtCMessage.Location = new System.Drawing.Point(20, 150);
+            this.txtCMessage.Location = new System.Drawing.Point(17, 163);
             this.txtCMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtCMessage.Multiline = true;
             this.txtCMessage.Name = "txtCMessage";
             this.txtCMessage.ReadOnly = true;
-            this.txtCMessage.Size = new System.Drawing.Size(726, 232);
+            this.txtCMessage.Size = new System.Drawing.Size(623, 251);
             this.txtCMessage.TabIndex = 16;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(17, 126);
+            this.label6.Location = new System.Drawing.Point(15, 137);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.Size = new System.Drawing.Size(55, 13);
             this.label6.TabIndex = 15;
             this.label6.Text = "Messages";
             // 
             // txtCTopic
             // 
-            this.txtCTopic.Location = new System.Drawing.Point(111, 85);
+            this.txtCTopic.Location = new System.Drawing.Point(95, 92);
             this.txtCTopic.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtCTopic.Name = "txtCTopic";
-            this.txtCTopic.Size = new System.Drawing.Size(635, 21);
+            this.txtCTopic.Size = new System.Drawing.Size(545, 20);
             this.txtCTopic.TabIndex = 14;
             // 
             // txtCServer
             // 
-            this.txtCServer.Location = new System.Drawing.Point(111, 53);
+            this.txtCServer.Location = new System.Drawing.Point(95, 57);
             this.txtCServer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtCServer.Name = "txtCServer";
-            this.txtCServer.Size = new System.Drawing.Size(635, 21);
+            this.txtCServer.Size = new System.Drawing.Size(545, 20);
             this.txtCServer.TabIndex = 13;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 88);
+            this.label4.Location = new System.Drawing.Point(15, 95);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(73, 12);
+            this.label4.Size = new System.Drawing.Size(63, 13);
             this.label4.TabIndex = 12;
             this.label4.Text = "Topic name";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 56);
+            this.label5.Location = new System.Drawing.Point(15, 61);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(75, 12);
+            this.label5.Size = new System.Drawing.Size(67, 13);
             this.label5.TabIndex = 11;
             this.label5.Text = "Kafka server";
             // 
             // btnExport
             // 
-            this.btnExport.Location = new System.Drawing.Point(624, 12);
+            this.btnExport.Location = new System.Drawing.Point(535, 13);
             this.btnExport.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnExport.Name = "btnExport";
-            this.btnExport.Size = new System.Drawing.Size(58, 22);
+            this.btnExport.Size = new System.Drawing.Size(50, 24);
             this.btnExport.TabIndex = 10;
             this.btnExport.Text = "Export";
             this.btnExport.UseVisualStyleBackColor = true;
@@ -242,10 +253,10 @@
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(560, 12);
+            this.btnClear.Location = new System.Drawing.Point(480, 13);
             this.btnClear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(58, 22);
+            this.btnClear.Size = new System.Drawing.Size(50, 24);
             this.btnClear.TabIndex = 9;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -253,10 +264,10 @@
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(496, 12);
+            this.btnStop.Location = new System.Drawing.Point(425, 13);
             this.btnStop.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(58, 22);
+            this.btnStop.Size = new System.Drawing.Size(50, 24);
             this.btnStop.TabIndex = 8;
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
@@ -264,10 +275,10 @@
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(432, 12);
+            this.btnStart.Location = new System.Drawing.Point(370, 13);
             this.btnStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(58, 22);
+            this.btnStart.Size = new System.Drawing.Size(50, 24);
             this.btnStart.TabIndex = 7;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -278,10 +289,10 @@
             this.chbConsumer.AutoSize = true;
             this.chbConsumer.Checked = true;
             this.chbConsumer.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chbConsumer.Location = new System.Drawing.Point(24, 16);
+            this.chbConsumer.Location = new System.Drawing.Point(21, 17);
             this.chbConsumer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chbConsumer.Name = "chbConsumer";
-            this.chbConsumer.Size = new System.Drawing.Size(130, 16);
+            this.chbConsumer.Size = new System.Drawing.Size(113, 17);
             this.chbConsumer.TabIndex = 0;
             this.chbConsumer.Text = "Same as Producer";
             this.chbConsumer.UseVisualStyleBackColor = true;
@@ -292,10 +303,10 @@
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.txtToolTip});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 420);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 457);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 12, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(772, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(662, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -305,22 +316,11 @@
             this.txtToolTip.Size = new System.Drawing.Size(48, 17);
             this.txtToolTip.Text = "DigiLog";
             // 
-            // btnEnd
-            // 
-            this.btnEnd.Location = new System.Drawing.Point(688, 12);
-            this.btnEnd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnEnd.Name = "btnEnd";
-            this.btnEnd.Size = new System.Drawing.Size(58, 22);
-            this.btnEnd.TabIndex = 17;
-            this.btnEnd.Text = "End";
-            this.btnEnd.UseVisualStyleBackColor = true;
-            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
-            // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(772, 442);
+            this.ClientSize = new System.Drawing.Size(662, 479);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tabMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
